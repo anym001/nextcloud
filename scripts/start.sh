36e71649e49d53fa118d3bcc85037b92e8a6e1f7
Christoph <christophhummer@gmail.com>
2022-07-13 09:40:41 +0200

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
-if [ -z "${MEMORY_LIMIT}" ]; 
-then
-    MEMORY_LIMIT="1024M"
+if [ -z "${MEMORY_LIMIT}" ]; then
+    echo memory_limit="1024M" > /usr/local/etc/php/conf.d/z-memory-limit.ini
 else
-    echo memory_limit=${MEMORY_LIMIT} > /usr/local/etc/php/conf.d/z-memory-limit.ini
+    echo "memory_limit=${MEMORY_LIMIT}" > /usr/local/etc/php/conf.d/z-memory-limit.ini
 fi
+
+# Start webserver, php, whatever,... add this here
